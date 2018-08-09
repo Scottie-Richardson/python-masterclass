b294142de2bd21ff49125e4565d3300a045b972c
Scottie Richardson <scottie.d.richardson@gmail.com>
2018-08-09 16:27:24 -0500

    Add downloaded source code for the course

---
+++ HelloWorld/helloWorld.py
@@ -0,0 +1,10 @@
+# print("Hello World!")
+# greeting = "Hello"
+# name = input("Please enter your name: ")
+# print(greeting + ' ' + name)  # Comments are done like so
+# print(10/5)
+# print(10//5)
+# print(10/3)
+# print(10//3)
+# print(10/6)
+# print(10//6)
